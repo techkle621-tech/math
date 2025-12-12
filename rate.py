586d7f22196c0d2a4ceaf1619b500d2c4991f6fa
techkle621-tech <techkle621@gmail.com>
2025-12-12 09:28:45 +0530

    rate.py

---
+++ rate.py
@@ -0,0 +1,10 @@
+try:
+a=float(input("enter first number:")
+b=float(input("enter first number:")
+print(f"Addition:{a+b}")
+print(f"Substraction:{a-b}")
+print(f"Multiplication{a*b}")
+except ValueError
+print("enter valid number")
+
+
